045bc610c8bd7a6fa69afd560cd763ea699f0f6d
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-30 19:59:44 +0200

    Added Color.java to rep, implemented attributes as well as Constructor. Also Getter for tests

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -4,8 +4,9 @@ package at.fhj.msd;
 public class App {
     public static void main(String[] args) {
 
-        
-        
+        Color c1 = new Color(2, 0, 0);
+        System.out.printf("red: %d; green: %d; blue: %d", c1.getRed(), c1.getGreen(), c1.getGetBlue());
+ 
 
     }
 }
---
+++ src/main/java/at/fhj/msd/Color.java
@@ -0,0 +1,30 @@
+package at.fhj.msd;
+
+public class Color {
+
+    private int red;
+    private int green;
+    private int blue;
+
+
+    public Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    //? Constructor
+
+    public int getRed() {
+        return this.red;
+    }
+
+    public int getGreen() {
+        return this.green;
+    }
+
+    public int getGetBlue() {
+        return this.blue;
+    }
+    
+}
