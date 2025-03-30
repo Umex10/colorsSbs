6682fbe12e1415107cc8e7bd1e53660e2df68935
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2025-03-30 20:59:03 +0200

    Overrided equals(obj) method inside Color.java and tested it

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -5,10 +5,19 @@ public class App {
     public static void main(String[] args) {
 
         Color c1 = new Color(2, 255, 0);
-        System.out.println(c1.rgbFormat());
-        System.out.println(c1.hexFormat());
+        System.out.printf("rgb: %s\n",c1.rgbFormat());
+        System.out.printf("rgb: %s\n", c1.hexFormat());
 
-        System.out.println(c1.toString());
+        System.out.printf("rgb with ToString(): %s\n",c1.toString());
+
+        Color c2 = new Color(2, 255, 0);
+
+        Color c3 = new Color(2,200,5);
+
+        System.out.printf("c1 = c2? --> %b\n", c1.equals(c2));
+        System.out.printf("c1 = c3? --> %b\n", c1.equals(c3));
+        System.out.printf("c2 = c3? --> %b", c2.equals(c3));
+
 
     }
 }
--- src/main/java/at/fhj/msd/Color.java
+++ src/main/java/at/fhj/msd/Color.java
@@ -1,16 +1,17 @@
 package at.fhj.msd;
 
+
 public class Color {
 
-    private int red;
-    private int green;
-    private int blue;
+    public int red;
+    public int green;
+    public int blue;
 
     public Color(int red, int green, int blue) {
 
         if ((red < 0 || green < 0 || blue < 0) || (red > 255 || green > 255 || blue > 255)) {
 
-           throw new IllegalArgumentException("Invalid values for the base colors");
+            throw new IllegalArgumentException("Invalid values for the base colors");
         }
 
         this.red = red;
@@ -42,6 +43,21 @@ public class Color {
     @Override
     public String toString() {
         return this.rgbFormat();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+
+            return true;
+        }
+        if (!(obj instanceof Color)) {
+            return false;
+        }
+
+        Color other = (Color) obj;
+        return this.red == other.red && this.green == other.green && this.blue == other.blue;
     }
 
 }
